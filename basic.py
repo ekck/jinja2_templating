d3c9f3e6bb5675d9f1d97dc39ad67c64b0a0e53c
kiprono <kiprono_emmanuel@yahoo.com>
2021-07-12 17:52:27 +0300

    super-blocks

---
+++ basic.py
@@ -0,0 +1,8 @@
+# python terminal basic examples
+from jinja2 import Template
+
+t = Template("Hello {{ something }}!")
+t.render(something="World")
+
+t = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
+t.render()
