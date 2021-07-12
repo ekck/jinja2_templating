5019f3d6993c45d8e7acbe8f5222ce4a1c0859c2
kiprono <kiprono_emmanuel@yahoo.com>
2021-07-12 18:26:39 +0300

    filters

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """Convert a datetime to a different format."""
+    return value.strftime(format)
 
-@app.route("/home")
+app.jinja_env.filters['datetimefilter'] = datetimefilter
+
+@app.route("/")
 def home():
     return render_template(
         'template.html', my_string="Wheeeee!", 
-        my_list=[0,1,2,3,4,5], title="Home")
+        my_list=[0,1,2,3,4,5], title="Home", current_time = datetime.datetime.now())
 
 @app.route("/about")
 def about():
